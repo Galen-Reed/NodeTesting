60bd2b7e5a14b77507423aacbd19c085f9c8a8fa
Galen-Reed <105329257+Galen-Reed@users.noreply.github.com>
2022-10-31 23:51:12 -0400

    Create demo_uppercase.js

---
+++ demo_uppercase.js
@@ -0,0 +1,6 @@
+var http = require('http');
+var uc = require('upper-case');
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end();
+}).listen(8080);
