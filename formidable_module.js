3167d3bf46e94f429fea839694108329cc16f336
Galen-Reed <105329257+Galen-Reed@users.noreply.github.com>
2022-11-02 01:15:27 -0400

    fs_module

--- formidable_module.js
+++ formidable_module.js
@@ -17,8 +17,8 @@ http.createServer(function (req, res) {
     } else {
         res.writeHead(200, {'Content-Type': 'text/html'});
         res.write('<form action="fileupload method="post" enctype="multipart/form-data">');
-        res.write('<input type="file" name="filetoupload"><br>');
-        res.write('<input type="submit">');
+        res.write('<input type="file" name="filetoupload" accept=".csv" ></input><br>');
+        res.write('<input type="submit"></input>');
         res.write('</form>');
         return res.end();
     }
